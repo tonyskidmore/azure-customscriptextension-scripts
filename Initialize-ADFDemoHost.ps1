48ffeb5745e15fd771845f717ce3e37f5871ce23
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2023-08-20 20:03:46 +0100

    fixing path

--- Initialize-ADFDemoHost.ps1
+++ Initialize-ADFDemoHost.ps1
@@ -11,10 +11,10 @@ $tempPath = [System.IO.Path]::GetTempPath()
 
 # Create a unique directory name
 $tempDir = Join-Path $tempPath ([System.Guid]::NewGuid().ToString())
-Set-Location -Path $tempDir
 
 # Create the directory
 New-Item -Path $tempDir -ItemType Directory
+Set-Location -Path $tempDir
 
 foreach ($url in $urls) {
     $output = "$tempDir\$(Split-Path -Path $url -Leaf)"
