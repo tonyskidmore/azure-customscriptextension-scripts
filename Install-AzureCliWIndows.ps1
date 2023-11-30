f277529daa47b0126b697811bb17b7539f533abc
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2023-11-30 17:25:16 +0000

    adding az cli install on windows

---
+++ Install-AzureCliWIndows.ps1
@@ -0,0 +1,4 @@
+Write-Host "Installing Azure CLI..."
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi
+Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
+Remove-Item .\AzureCLI.msi
